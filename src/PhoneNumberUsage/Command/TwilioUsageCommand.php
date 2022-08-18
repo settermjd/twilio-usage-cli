727da2ce3c2456a4d35e49ea3d763a77d97a0417
Matthew Setter <matthew@matthewsetter.com>
2022-08-18 10:58:32 +0200

    Refine how the account information is formatted
    This change uses a series of classes to format the cost information so
    that it's more meaningful and renders as people would expect currency
    data to render.

--- src/PhoneNumberUsage/Command/TwilioUsageCommand.php
+++ src/PhoneNumberUsage/Command/TwilioUsageCommand.php
@@ -8,21 +8,16 @@ use Money\Currencies\ISOCurrencies;
 use Money\Currency;
 use Money\Formatter\IntlMoneyFormatter;
 use Money\Money;
+use NumberFormatter;
 use PhoneNumberUsage\TwilioUsage;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Helper\TableCell;
 use Symfony\Component\Console\Helper\TableSeparator;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Twilio\Deserialize;
-use Twilio\Rest\Accounts;
-use Twilio\Rest\Api\V2010\Account\Usage\RecordInstance;
-use Twilio\Rest\Client;
-use Twilio\Version;
 
 class TwilioUsageCommand extends Command
 {
@@ -30,21 +25,29 @@ class TwilioUsageCommand extends Command
 
     protected static $defaultDescription = "Lists a Twilio account's usage details.";
 
-    protected string $helpMessage = <<<EOF
+    protected string $helpMessage
+        = <<<EOF
 The command lists a Twilio account's usage details.
 It supports the ability to list account usage for SMS, MMS, and voice calls within a given date range, and filter by usage categories (daily, monthly, today, yesterday, etc).
 EOF;
 
     private TwilioUsage $twilioUsage;
     private array $rows = [];
-    private \NumberFormatter $formatter;
+    private NumberFormatter $formatter;
+    private IntlMoneyFormatter $moneyFormatter;
 
     public function __construct(TwilioUsage $twilioUsage)
     {
         parent::__construct();
 
-        $this->formatter = new \NumberFormatter('en_US', \NumberFormatter::PATTERN_DECIMAL);
         $this->twilioUsage = $twilioUsage;
+        $currencies = new ISOCurrencies();
+        $this->formatter = new NumberFormatter(
+            'en_US', NumberFormatter::CURRENCY
+        );
+        $this->moneyFormatter = new IntlMoneyFormatter(
+            $this->formatter, $currencies
+        );
     }
 
     protected function configure(): void
@@ -54,7 +57,7 @@ EOF;
         $this->addOption(
             'limit-records',
             'l',
-        InputOption::VALUE_OPTIONAL,
+            InputOption::VALUE_OPTIONAL,
             'The record limit'
         );
         $this->addOption(
@@ -77,8 +80,8 @@ EOF;
         );
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
+    protected function execute(InputInterface $input, OutputInterface $output
+    ): int {
         $outputStyle = new OutputFormatterStyle('#56be4e', null, ['bold']);
         $output->getFormatter()->setStyle('fire', $outputStyle);
 
@@ -87,12 +90,54 @@ EOF;
         $category = $input->getOption('category') ?: null;
         $limitRecords = (int)$input->getOption('limit-records') ?: 20;
 
+        $totalCost = $this->getUsageData(
+            $limitRecords, $startDate, $endDate, $category
+        );
+
+        $total = new Money((int)round($totalCost * 100), new Currency('USD'));
+
+        $this->rows[] = new TableSeparator();
+        $this->rows[] = [
+            new TableCell(
+                sprintf(
+                    'Total records: %d. Total cost: %s.',
+                    count($this->rows) - 1,
+                    $this->moneyFormatter->format($total)
+                ),
+                ['colspan' => 4]
+            )
+        ];
+
+        $output->writeln("");
+        $output->writeln("<fire>Twilio Account Usage Statistics</>\n");
+        if ($filterNotice = $this->getFilterNotification(
+            $startDate, $endDate, $category
+        )
+        ) {
+            $output->writeln($filterNotice . "\n");
+        }
+
+
+        $table = new Table($output);
+        $table
+            ->setHeaders(['Date', 'Category', 'Price', 'Currency'])
+            ->setRows($this->rows);
+        $table->render();
+
+        return Command::SUCCESS;
+    }
+
+    public function getUsageData(
+        int $limitRecords,
+        string $startDate,
+        string $endDate,
+        string $category
+    ): string|float {
         $records = $this->twilioUsage->__invoke(
             $limitRecords, $startDate, $endDate, $category
         );
 
         $totalCost = 0.00;
-
         foreach ($records as $record) {
             $this->rows[] = [
                 $record->asOf,
@@ -107,37 +152,12 @@ EOF;
             $totalCost += $record->price;
         }
 
-        $total = new Money((int) round($totalCost * 100), new Currency('USD'));
-        $currencies = new ISOCurrencies();
-        $numberFormatter = new \NumberFormatter('en_US', \NumberFormatter::CURRENCY);
-        $moneyFormatter = new IntlMoneyFormatter($numberFormatter, $currencies);
-
-        $this->rows[] = new TableSeparator();
-        $this->rows[] = [
-            new TableCell(
-                sprintf('Total records: %d. Total cost: %s.', count($this->rows) -1, $moneyFormatter->format($total)),
-                ['colspan' => 4]
-            )
-        ];
-
-        $output->writeln("");
-        $output->writeln("<fire>Twilio Account Usage Statistics</>\n");
-        if ($filterNotice = $this->getFilterNotification($startDate, $endDate, $category)) {
-            $output->writeln($filterNotice . "\n");
-        }
-
-
-        $table = new Table($output);
-        $table
-            ->setHeaders(['Date', 'Category', 'Price', 'Currency'])
-            ->setRows($this->rows);
-        $table->render();
-
-        return Command::SUCCESS;
+        return $totalCost;
     }
 
-    protected function getFilterNotification(string $startDate = null, string $endDate = null, string $category = null): string
-    {
+    protected function getFilterNotification(string $startDate = null,
+        string $endDate = null, string $category = null
+    ): string {
         $output = "";
 
         if ($startDate === null && $endDate === null && $category === null) {
